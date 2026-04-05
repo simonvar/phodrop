0a37aff960f37b2e92cfb1faffb3da1c0edc54ca
simonvar <simonvar@protonmail.com>
2026-04-05 20:56:44 +0300

    Release pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.simonvar.photodrop"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
